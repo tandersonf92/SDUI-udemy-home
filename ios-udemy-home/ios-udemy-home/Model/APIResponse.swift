351ea0807c557f2b813ef5e9693d8db59b86a847
Anderson Oliveira <andersonoliveira@Mac-mini.local>
2026-03-31 10:51:47 -0300

    lesson 4: add ui model

--- ios-udemy-home/ios-udemy-home/Model/APIResponse.swift
+++ ios-udemy-home/ios-udemy-home/Model/APIResponse.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct HomeUIModel: Hashable {
+   struct SectionModel: Hashable {
+      let section: Section
+      let body: [Item]
+   }
+   
+   enum Section: Hashable {
+      case mainBanner(id: String)
+      case textHeader(id: String)
+      case courseSwimlane(id: String)
+      case udemyBusinessBanner(id: String)
+      case categories(id: String)
+      case featureCourse(id: String)
+   }
+   
+   enum Item: Hashable {
+      case mainBanner(id: String, imageLink: String, title: String, caption: String)
+      case couse(id: String, imageLink: String, title: String, author: String, rating: Double, reviewCount: Int, price: Decimal, tag: String)
+      case textHeader(id: String, text: String, highlightedText: String?)
+      case udemyBusinessBanner(id: String, link: String)
+      case categoriesScroller(id: String, titles: [String])
+      case featureCourse(id: String, imageLink: String, title: String, author: String, rating: Double, reviewCount: Int, price: Decimal)
+   }
+}

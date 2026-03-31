1c84f0ac6658fa3febd849cc8fc925af0c7590ea
Anderson Oliveira <andersonoliveira@Mac-mini.local>
2026-03-31 10:01:38 -0300

    Initial commit

---
+++ ios-udemy-home/ios-udemy-home/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-udemy-home
+//
+//  Created by Anderson Oliveira on 31/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+      view.backgroundColor = .red
+   }
+
+
+}
+
